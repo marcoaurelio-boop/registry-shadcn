271d8b34c4a9a2fe2def2d5a989aed920900619f
Marco Aurélio <marco.aurelio@sydle.com>
2025-10-30 12:54:33 -0300

    organize the theme

--- src/app/(registry)/layout.tsx
+++ src/app/(registry)/layout.tsx
@@ -16,7 +16,7 @@ export default function RegistryLayout({
     <SidebarProvider>
       <MobileSidebarTrigger />
       <RegistrySidebar />
-      <main className="flex w-full justify-center">{children}</main>
+      <main className="catlife-theme flex w-full justify-center">{children}</main>
       <Toaster />
     </SidebarProvider>
   );
